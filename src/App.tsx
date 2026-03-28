18918c24aa287a75fcad7bc247df82feb14f63ec
GXVisuals <giorgos.xaritonos@gmail.com>
2026-03-28 15:31:41 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-// Αρχικοποίηση του QueryClient για τη διαχείριση δεδομένων
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      {/* Εξαρτήματα για τα αναδυόμενα μηνύματα (Notifications) */}
       <Toaster />
       <Sonner />
-      
       <BrowserRouter>
         <Routes>
-          {/* Η κεντρική σελίδα (Home) */}
           <Route path="/" element={<Index />} />
-          
-          {/* Σελίδα 404 - αν ο χρήστης γράψει λάθος URL */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>

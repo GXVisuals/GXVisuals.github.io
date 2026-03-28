3bbc2796a63529dca4d7eccee2e42603750c1d12
GXVisuals <giorgos.xaritonos@gmail.com>
2026-03-28 16:42:10 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Instagram, Music2, Facebook, Phone } from "lucide-react"; 
 import { useTranslation } from 'react-i18next';
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { NavLink } from "@/components/ui/NavLink";
+import { NavLink } from "@/components/NavLink";
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);

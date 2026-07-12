5991a200a58c81b6201adfa1df30790710f89340
GXVisuals <giorgos.xaritonos@gmail.com>
2026-07-12 15:07:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Lazy loading για να μην φορτώνει το portfolio αν ο χρήστης μείνει στην αρχική
 const PortfolioPage = lazy(() => import("./pages/PortfolioPage"));
 
 const queryClient = new QueryClient();
